3f2662fdd9d127cbbdc64cd0ff86eb1cf4088dd4
Tany <1173904005@qq.com>
2022-08-10 17:55:36 +0800

    优化搜索与增加

--- skiplist/skipList.go
+++ skiplist/skipList.go
@@ -102,9 +102,9 @@ func (sl *SkipList) headNodeInit() {
 }
 
 //更新当前最大层数
-func (sl *SkipList) updateCurrentMaxLevel() {
+func (sl *SkipList) updateCurrentMaxLevel(currentLevel int) {
 	if sl.length > 0 {
-		for level := sl.constMaxLevel; level >= 0; level-- {
+		for level := currentLevel; level >= 0; level-- {
 			if sl.head.level[level].next != nil {
 				sl.currentMaxLevel = level
 				return
@@ -156,19 +156,14 @@ func (sl *SkipList) searchTailOneByKey(key interface{}) *skipListNode {
 //获取任意一个,只要找到相等的就返回
 func (sl *SkipList) searchRandOneByKey(key interface{}) *skipListNode {
 	if sl.length > 0 {
-		var node *skipListNode
+		preNode := sl.head
 		for level := sl.currentMaxLevel; level >= 0; level-- {
-			if node = sl.head.level[level].next; node != nil && sl.lessOrEquals(node.key, key) {
-				for ; level >= 0; level-- {
-					for {
-						if sl.equals(node.key, key) {
-							return node
-						} else if node.level[level].next != nil && sl.lessOrEquals(node.level[level].next.key, key) {
-							node = node.level[level].next
-						} else {
-							break
-						}
+			for ; ; preNode = preNode.level[level].next {
+				if preNode.level[level].next == nil || sl.greaterThan(preNode.level[level].next.key, key) {
+					if preNode != sl.head && sl.equals(preNode.key, key) {
+						return preNode
 					}
+					break
 				}
 			}
 		}
@@ -185,7 +180,7 @@ func (sl *SkipList) searchByRankRange(start, end int) []*skipListNode {
 	if start == sl.length {
 		list = append(list, sl.tail)
 	} else if end == sl.length {
-		for node := sl.tail; start <= end && end >= 1; end, node = end-1, node.prev {
+		for node := sl.tail; node != nil && start <= end && end >= 1; end, node = end-1, node.prev {
 			list = append(list, node)
 		}
 		sl.reverse(list)
@@ -194,7 +189,7 @@ func (sl *SkipList) searchByRankRange(start, end int) []*skipListNode {
 		if start > 1 {
 			node = sl.searchByRank(start)
 		}
-		for ; start <= end && start <= sl.length && node != nil; start, node = start+1, node.level[0].next {
+		for ; node != nil && start <= end && start <= sl.length; start, node = start+1, node.level[0].next {
 			list = append(list, node)
 		}
 	}
@@ -203,36 +198,30 @@ func (sl *SkipList) searchByRankRange(start, end int) []*skipListNode {
 
 //通过精确rank搜索
 func (sl *SkipList) searchByRank(rk int) *skipListNode {
-	if rk > sl.length || rk < 1 {
-		return nil
-	}
-	if rk == 1 {
-		return sl.head.level[0].next
-	} else if rk == sl.length {
-		return sl.tail
-	} else {
+	if rk > 0 && rk <= sl.length {
+		if rk == 1 {
+			return sl.head.level[0].next
+		} else if rk == sl.length {
+			return sl.tail
+		}
+		currentRank := 0
+		preNode := sl.head
 		for level := sl.currentMaxLevel; level >= 0; level-- {
-			if node := sl.head.level[level].next; node != nil && sl.head.level[level].span <= rk {
-				currentRank := sl.head.level[level].span
-				for ; level >= 0; level-- {
-					for {
-						if currentRank == rk {
-							return node
-						} else if node.level[level].next != nil && node.level[level].span+currentRank <= rk {
-							currentRank += node.level[level].span
-							node = node.level[level].next
-						} else {
-							break
-						}
+			for ; ; preNode = preNode.level[level].next {
+				if preNode.level[level].next == nil || preNode.level[level].span+currentRank > rk {
+					if currentRank == rk {
+						return preNode
 					}
+					break
 				}
+				currentRank += preNode.level[level].span
 			}
 		}
 	}
 	return nil
 }
 
-//TODO 添加结点
+//添加结点
 func (sl *SkipList) addNode(key, data interface{}) {
 	addNode := sl.nodeGenerate(key, data)
 	if sl.length == 1 { //generate +1 了
@@ -247,99 +236,109 @@ func (sl *SkipList) addNode(key, data interface{}) {
 	nextL := make([]*skipListNode, len(addNode.level)) // [层数]后置结点
 	nrm := map[*skipListNode]int{}                     // [结点:rank]
 	nodeRank := 0                                      //当前结点rank
-	var preNode *skipListNode
+	var preNode *skipListNode = sl.head
+	//找前置与后置结点，并记录rank
 	for level := sl.currentMaxLevel; level >= 0; level-- {
-		if preNode = sl.head.level[level].next; preNode != nil {
-			if sl.lessOrEquals(preNode.key, addNode.key) {
-				nodeRank = sl.head.level[level].span
-				for ; level >= 0; level-- {
-					if sl.lessOrEquals(preNode.key, key) &&
-						(preNode.level[level].next == nil || sl.greaterThan(preNode.level[level].next, key)) {
-						addNode.prev = preNode
-						addNode.level[level].next = preNode.level[level].next
-						preNode.level[level].next = addNode
-						addNode.level[level].next.prev = addNode
-						nextL[level] = addNode.level[level].next
-						nrm[preNode] = nodeRank
-						nrm[addNode.level[level].next] = nodeRank + 1 + preNode.level[level].span
-					} else if sl.lessOrEquals(preNode.key, key) {
-						nodeRank += preNode.level[level].span
-						preNode = preNode.level[level].next
-					} else {
-						prevL[level] = preNode
-						nrm[preNode] = nodeRank
-						preNode.level[level].next = addNode
+		for {
+			if preNode.level[level].next == nil || sl.greaterThan(preNode.level[level].next.key, key) {
+				if len(addNode.level) <= level {
+					if preNode.level[level].next != nil {
+						preNode.level[level].span++
+					}
+				} else {
+					prevL[level] = preNode
+					nrm[preNode] = nodeRank
+					if preNode.level[level].next != nil {
+						nextL[level] = preNode.level[level].next
+						nrm[preNode.level[level].next] = preNode.level[level].span + nodeRank + 1
 					}
 				}
-			} else if level < len(addNode.level) {
-				nextL[level] = preNode
-				prevL[level] = sl.head
-				nextL[level] = preNode
+				break
+			} else {
+				nodeRank += preNode.level[level].span
+				preNode = preNode.level[level].next
 			}
-		} else if level < len(addNode.level) {
-			prevL[level] = sl.head
 		}
 	}
-
-	//更新span
-
+	//当前结点的rank
+	nodeRank++
+	//更新前后置指向结点及本结点span
+	for level := len(addNode.level) - 1; level >= 0; level-- {
+		prevL[level].level[level].span = nodeRank - nrm[prevL[level]]
+		addNode.level[level].next = prevL[level].level[level].next
+		prevL[level].level[level].next = addNode
+		addNode.prev = prevL[level]
+		if nextL[level] != nil {
+			addNode.level[level].span = nrm[nextL[level]] - nodeRank
+			nextL[level].prev = addNode
+		}
+	}
+	//更新tail
+	if sl.tail == nil || sl.tail.level[0].next != nil {
+		sl.tail = addNode
+	}
 }
 
-//更新结点span (从该结点向两边扩散)
-func (sl *skipListNode) updateSpan(node *skipListNode) {
-
-}
-
-// 删除结点
+//删除结点
 func (sl *SkipList) delNode(delNode *skipListNode) {
 	if sl.length == 0 || delNode == nil {
 		return
 	}
 	defer func(sl *SkipList) {
 		sl.length--
-		if len(delNode.level) == sl.currentMaxLevel {
-			sl.updateCurrentMaxLevel()
+		if len(delNode.level)-1 >= sl.currentMaxLevel {
+			sl.updateCurrentMaxLevel(sl.currentMaxLevel)
 		}
 	}(sl)
 	if sl.tail == delNode {
 		sl.tail = delNode.prev
 	}
-	var preNode *skipListNode
+	preNode := sl.head
 	for level := sl.currentMaxLevel; level >= 0; level-- {
-		if preNode = sl.head.level[level].next; preNode != nil {
-			if preNode == delNode {
-				sl.head.level[level].next = delNode.level[level].next
-				delNode.level[level].next.prev = nil
-				sl.head.level[level].span += delNode.level[level].span - 1
-				if delNode.level[level].next == nil {
-					sl.head.level[level].span = 0
-				}
-			} else if sl.lessOrEquals(preNode.key, delNode.key) {
-				for ; level >= 0; level-- {
-				currentLevelNext:
-					if preNode.level[level].next != nil {
-						if preNode.level[level].next == delNode {
-							preNode.level[level].next = delNode.level[level].next
-							delNode.level[level].next.prev = preNode
-							preNode.level[level].span += delNode.level[level].span - 1
-							if delNode.level[level].next == nil {
-								preNode.level[level].span = 0
-							}
-						} else {
-							if sl.lessOrEquals(preNode.level[level].next.key, delNode.key) {
-								preNode = preNode.level[level].next
-								goto currentLevelNext
-							} else {
-								preNode.level[level].span--
-							}
-						}
-					}
-				}
-			} else {
-				sl.head.level[level].span--
+		for ; ; preNode = preNode.level[level].next {
+			if preNode.level[level].next == nil ||
+				(sl.greaterThan(preNode.level[level].next.key, delNode.key) && preNode.level[level].next != delNode) {
+
+				break
 			}
 		}
 	}
+
+	// for level := sl.currentMaxLevel; level >= 0; level-- {
+	// 	if preNode = sl.head.level[level].next; preNode != nil {
+	// 		if preNode == delNode {
+	// 			sl.head.level[level].next = delNode.level[level].next
+	// 			delNode.level[level].next.prev = nil
+	// 			sl.head.level[level].span += delNode.level[level].span - 1
+	// 			if delNode.level[level].next == nil {
+	// 				sl.head.level[level].span = 0
+	// 			}
+	// 		} else if sl.lessOrEquals(preNode.key, delNode.key) {
+	// 			for ; level >= 0; level-- {
+	// 			currentLevelNext:
+	// 				if preNode.level[level].next != nil {
+	// 					if preNode.level[level].next == delNode {
+	// 						preNode.level[level].next = delNode.level[level].next
+	// 						delNode.level[level].next.prev = preNode
+	// 						preNode.level[level].span += delNode.level[level].span - 1
+	// 						if delNode.level[level].next == nil {
+	// 							preNode.level[level].span = 0
+	// 						}
+	// 					} else {
+	// 						if sl.lessOrEquals(preNode.level[level].next.key, delNode.key) {
+	// 							preNode = preNode.level[level].next
+	// 							goto currentLevelNext
+	// 						} else {
+	// 							preNode.level[level].span--
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		} else {
+	// 			sl.head.level[level].span--
+	// 		}
+	// 	}
+	// }
 }
 
 //a,b相同
